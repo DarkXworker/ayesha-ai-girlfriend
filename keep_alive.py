181d4f8d9118aee54fb729bb7e4d2d94afeda585
Sudipx <161097158+DarkXworker@users.noreply.github.com>
2025-10-30 09:28:58 +0530

    Create keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import threading
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "🤖 Ayesha Bot is Alive! 💕"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = threading.Thread(target=run)
+    t.daemon = True
+    t.start()
+  
